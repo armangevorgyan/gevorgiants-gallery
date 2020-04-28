264ded7b84fa58d83d0c1948a5092f76298d741b
arman.gevorgyan <arman.gevorgyan@sflpro.com>
2020-04-29 00:50:55 +0400

    Added Exteriors/Interiors names

--- app/src/configs/gallery/exterior-interior.js
+++ app/src/configs/gallery/exterior-interior.js
@@ -3,7 +3,7 @@ import {assetGallery} from 'helpers/apiEndpoint';
 export const ExteriorInteriors = [
   {
     uuid: '775cc8c5-8517-44e5-b410-ab2d6d4aa188',
-    title: '1',
+    title: '"Cactus" Mexican restaurant',
     description: '',
     src: assetGallery('exterior-interior/images/01.jpg'),
     thumbnailSrc: assetGallery('exterior-interior/thumbnails/01.jpg'),
@@ -11,7 +11,7 @@ export const ExteriorInteriors = [
   },
   {
     uuid: '185910fc-fa9e-416f-b11f-a81cf5bb83bb',
-    title: '2',
+    title: '"Cactus" Mexican restaurant',
     description: '',
     src: assetGallery('exterior-interior/images/02.jpg'),
     thumbnailSrc: assetGallery('exterior-interior/thumbnails/02.jpg'),
@@ -19,7 +19,7 @@ export const ExteriorInteriors = [
   },
   {
     uuid: '8de40063-d1b5-4059-b067-899f4f3f8537',
-    title: '3',
+    title: '"Cactus" Mexican restaurant',
     description: '',
     src: assetGallery('exterior-interior/images/03.jpg'),
     thumbnailSrc: assetGallery('exterior-interior/thumbnails/03.jpg'),
@@ -27,7 +27,7 @@ export const ExteriorInteriors = [
   },
   {
     uuid: 'ed4cb4ad-9f77-49a4-be88-f387e98ad81a',
-    title: '4',
+    title: '"Cactus" Mexican restaurant',
     description: '',
     src: assetGallery('exterior-interior/images/04.jpg'),
     thumbnailSrc: assetGallery('exterior-interior/thumbnails/04.jpg'),
@@ -35,7 +35,7 @@ export const ExteriorInteriors = [
   },
   {
     uuid: '69eaa8fc-23b8-41f9-888b-711119a307ab',
-    title: '5',
+    title: '"Cactus" Mexican restaurant',
     description: '',
     src: assetGallery('exterior-interior/images/05.jpg'),
     thumbnailSrc: assetGallery('exterior-interior/thumbnails/05.jpg'),
@@ -43,7 +43,7 @@ export const ExteriorInteriors = [
   },
   {
     uuid: '61c5502f-67b0-4ce9-a4fc-eacea5ce67ea',
-    title: '6',
+    title: '"Cactus" Mexican restaurant',
     description: '',
     src: assetGallery('exterior-interior/images/06.jpg'),
     thumbnailSrc: assetGallery('exterior-interior/thumbnails/06.jpg'),
@@ -51,7 +51,7 @@ export const ExteriorInteriors = [
   },
   {
     uuid: '3c5e1a87-a546-4688-a8ed-7b4898115147',
-    title: '7',
+    title: '"Cactus" Mexican restaurant',
     description: '',
     src: assetGallery('exterior-interior/images/07.jpg'),
     thumbnailSrc: assetGallery('exterior-interior/thumbnails/07.jpg'),
@@ -59,7 +59,7 @@ export const ExteriorInteriors = [
   },
   {
     uuid: '7467838f-3f89-453d-a0f4-cdf5fc745ec6',
-    title: '8',
+    title: '"Cactus" Mexican restaurant',
     description: '',
     src: assetGallery('exterior-interior/images/08.jpg'),
     thumbnailSrc: assetGallery('exterior-interior/thumbnails/08.jpg'),
@@ -67,7 +67,7 @@ export const ExteriorInteriors = [
   },
   {
     uuid: 'e9108ee4-57ca-4c43-bc6a-ef0bb13f50a2',
-    title: '9',
+    title: '"Cactus" Mexican restaurant',
     description: '',
     src: assetGallery('exterior-interior/images/09.jpg'),
     thumbnailSrc: assetGallery('exterior-interior/thumbnails/09.jpg'),
@@ -75,7 +75,7 @@ export const ExteriorInteriors = [
   },
   {
     uuid: 'f2cb607b-b6cd-4faf-9fc0-a709ef647cb9',
-    title: '10',
+    title: 'Charles cafe & restaurant',
     description: '',
     src: assetGallery('exterior-interior/images/010.jpg'),
     thumbnailSrc: assetGallery('exterior-interior/thumbnails/010.jpg'),
@@ -83,7 +83,7 @@ export const ExteriorInteriors = [
   },
   {
     uuid: 'c974be72-379a-4f6a-b188-af9ffbf4e6a2',
-    title: '11',
+    title: 'Charles cafe & restaurant',
     description: '',
     src: assetGallery('exterior-interior/images/011.jpg'),
     thumbnailSrc: assetGallery('exterior-interior/thumbnails/011.jpg'),
@@ -91,7 +91,7 @@ export const ExteriorInteriors = [
   },
   {
     uuid: 'b805b3df-9c66-402c-8359-d1163d2071eb',
-    title: '12',
+    title: 'Charles cafe & restaurant',
     description: '',
     src: assetGallery('exterior-interior/images/012.jpg'),
     thumbnailSrc: assetGallery('exterior-interior/thumbnails/012.jpg'),
@@ -99,7 +99,7 @@ export const ExteriorInteriors = [
   },
   {
     uuid: '812af4bb-7bd7-4c7e-bf7b-fcfa7c5d6aad',
-    title: '13',
+    title: 'Charles cafe & restaurant',
     description: '',
     src: assetGallery('exterior-interior/images/013.jpg'),
     thumbnailSrc: assetGallery('exterior-interior/thumbnails/013.jpg'),
@@ -107,7 +107,7 @@ export const ExteriorInteriors = [
   },
   {
     uuid: '7d8a08db-1967-4211-81a6-9073141bd4e1',
-    title: '14',
+    title: 'Dali restaurant',
     description: '',
     src: assetGallery('exterior-interior/images/014.jpg'),
     thumbnailSrc: assetGallery('exterior-interior/thumbnails/014.jpg'),
@@ -115,7 +115,7 @@ export const ExteriorInteriors = [
   },
   {
     uuid: 'c878a5c9-9249-456b-a98a-ed764aed9492',
-    title: '15',
+    title: 'Dali restaurant',
     description: '',
     src: assetGallery('exterior-interior/images/015.jpg'),
     thumbnailSrc: assetGallery('exterior-interior/thumbnails/015.jpg'),
@@ -123,7 +123,7 @@ export const ExteriorInteriors = [
   },
   {
     uuid: 'db6f2c95-0122-4dfd-bbea-a740466779aa',
-    title: '16',
+    title: 'Dali restaurant',
     description: '',
     src: assetGallery('exterior-interior/images/016.jpg'),
     thumbnailSrc: assetGallery('exterior-interior/thumbnails/016.jpg'),
@@ -131,7 +131,7 @@ export const ExteriorInteriors = [
   },
   {
     uuid: '64d42d5a-6601-4a70-9a19-917b5b2f1b13',
-    title: '17',
+    title: 'Dali restaurant',
     description: '',
     src: assetGallery('exterior-interior/images/017.jpg'),
     thumbnailSrc: assetGallery('exterior-interior/thumbnails/017.jpg'),
@@ -139,7 +139,7 @@ export const ExteriorInteriors = [
   },
   {
     uuid: '6b35e43d-da21-4757-a351-8f9c68df63d6',
-    title: '18',
+    title: 'Dali restaurant',
     description: '',
     src: assetGallery('exterior-interior/images/018.jpg'),
     thumbnailSrc: assetGallery('exterior-interior/thumbnails/018.jpg'),
@@ -147,7 +147,7 @@ export const ExteriorInteriors = [
   },
   {
     uuid: 'fe0da7b7-1f8b-4e47-9280-e9d791f2fd3c',
-    title: '19',
+    title: 'Dali restaurant',
     description: '',
     src: assetGallery('exterior-interior/images/019.jpg'),
     thumbnailSrc: assetGallery('exterior-interior/thumbnails/019.jpg'),
@@ -155,7 +155,7 @@ export const ExteriorInteriors = [
   },
   {
     uuid: '9adfe7e8-3aa9-49ee-b918-5ef0dfbecdf6',
-    title: '20',
+    title: 'Dali restaurant',
     description: '',
     src: assetGallery('exterior-interior/images/020.jpg'),
     thumbnailSrc: assetGallery('exterior-interior/thumbnails/020.jpg'),
@@ -163,7 +163,7 @@ export const ExteriorInteriors = [
   },
   {
     uuid: '705c3f93-c17c-4495-ad69-1ea3b6199106',
-    title: '21',
+    title: 'Private house',
     description: '',
     src: assetGallery('exterior-interior/images/021.jpg'),
     thumbnailSrc: assetGallery('exterior-interior/thumbnails/021.jpg'),
@@ -171,7 +171,7 @@ export const ExteriorInteriors = [
   },
   {
     uuid: '3d3b0912-f397-4c19-9d81-289285ca37ca',
-    title: '22',
+    title: 'Private house',
     description: '',
     src: assetGallery('exterior-interior/images/022.jpg'),
     thumbnailSrc: assetGallery('exterior-interior/thumbnails/022.jpg'),
@@ -179,7 +179,7 @@ export const ExteriorInteriors = [
   },
   {
     uuid: '6f642ae5-61bc-4dbc-9100-2926eb0357be',
-    title: '23',
+    title: 'Private house',
     description: '',
     src: assetGallery('exterior-interior/images/023.jpg'),
     thumbnailSrc: assetGallery('exterior-interior/thumbnails/023.jpg'),
@@ -187,7 +187,7 @@ export const ExteriorInteriors = [
   },
   {
     uuid: '707e98fd-e2ba-4900-9bea-ddb09d023a1d',
-    title: '24',
+    title: 'Pharaon restaurant',
     description: '',
     src: assetGallery('exterior-interior/images/024.jpg'),
     thumbnailSrc: assetGallery('exterior-interior/thumbnails/024.jpg'),
@@ -195,7 +195,7 @@ export const ExteriorInteriors = [
   },
   {
     uuid: 'b2e4f4f2-7640-4293-a86e-71734f82120e',
-    title: '25',
+    title: 'Pharaon restaurant',
     description: '',
     src: assetGallery('exterior-interior/images/025.jpg'),
     thumbnailSrc: assetGallery('exterior-interior/thumbnails/025.jpg'),
@@ -203,7 +203,7 @@ export const ExteriorInteriors = [
   },
   {
     uuid: '440484c5-e2f4-4122-b098-5cc330678eb4',
-    title: '26',
+    title: 'Pharaon restaurant',
     description: '',
     src: assetGallery('exterior-interior/images/026.jpg'),
     thumbnailSrc: assetGallery('exterior-interior/thumbnails/026.jpg'),
@@ -211,7 +211,7 @@ export const ExteriorInteriors = [
   },
   {
     uuid: '08db958b-d9ef-4cbb-89a8-0487e19e252d',
-    title: '27',
+    title: 'Kilikia restaurant',
     description: '',
     src: assetGallery('exterior-interior/images/027.jpg'),
     thumbnailSrc: assetGallery('exterior-interior/thumbnails/027.jpg'),
@@ -219,7 +219,7 @@ export const ExteriorInteriors = [
   },
   {
     uuid: '82a00837-776c-49e6-9530-94ffe5817735',
-    title: '28',
+    title: 'Kilikia restaurant',
     description: '',
     src: assetGallery('exterior-interior/images/028.jpg'),
     thumbnailSrc: assetGallery('exterior-interior/thumbnails/028.jpg'),
@@ -227,7 +227,7 @@ export const ExteriorInteriors = [
   },
   {
     uuid: 'c1acbc20-2839-4a66-99ef-0bffb75d4e7e',
-    title: '29',
+    title: 'Marco Polo restaurant',
     description: '',
     src: assetGallery('exterior-interior/images/029.jpg'),
     thumbnailSrc: assetGallery('exterior-interior/thumbnails/029.jpg'),
@@ -235,7 +235,7 @@ export const ExteriorInteriors = [
   },
   {
     uuid: '42bbcad4-ecd7-46e8-b35f-0af71a42bb54',
-    title: '30',
+    title: 'Marco Polo restaurant',
     description: '',
     src: assetGallery('exterior-interior/images/030.jpg'),
     thumbnailSrc: assetGallery('exterior-interior/thumbnails/030.jpg'),
@@ -243,7 +243,7 @@ export const ExteriorInteriors = [
   },
   {
     uuid: '4c8e3f76-d5bf-42f4-b09f-fe829f3f3154',
-    title: '31',
+    title: 'Gaudi hall',
     description: '',
     src: assetGallery('exterior-interior/images/031.jpg'),
     thumbnailSrc: assetGallery('exterior-interior/thumbnails/031.jpg'),
@@ -251,7 +251,7 @@ export const ExteriorInteriors = [
   },
   {
     uuid: 'ce52c034-77fe-425f-8f8f-c9cba74a50e9',
-    title: '32',
+    title: 'Gaudi hall',
     description: '',
     src: assetGallery('exterior-interior/images/032.jpg'),
     thumbnailSrc: assetGallery('exterior-interior/thumbnails/032.jpg'),
@@ -259,7 +259,7 @@ export const ExteriorInteriors = [
   },
   {
     uuid: 'd983e661-a641-417c-b9f4-b3b4e998231e',
-    title: '33',
+    title: 'Sherlock Holmes restaurant',
     description: '',
     src: assetGallery('exterior-interior/images/033.jpg'),
     thumbnailSrc: assetGallery('exterior-interior/thumbnails/033.jpg'),
@@ -267,7 +267,7 @@ export const ExteriorInteriors = [
   },
   {
     uuid: 'bd5fbb84-1e3d-4f0c-9354-984690794e1d',
-    title: '34',
+    title: 'Samurai sushi bar',
     description: '',
     src: assetGallery('exterior-interior/images/034.jpg'),
     thumbnailSrc: assetGallery('exterior-interior/thumbnails/034.jpg'),
@@ -275,7 +275,7 @@ export const ExteriorInteriors = [
   },
   {
     uuid: 'beed7eaa-fa8e-4845-98cb-38dcd5a1f386',
-    title: '35',
+    title: 'Amazon restaurant',
     description: '',
     src: assetGallery('exterior-interior/images/035.jpg'),
     thumbnailSrc: assetGallery('exterior-interior/thumbnails/035.jpg'),
@@ -283,7 +283,7 @@ export const ExteriorInteriors = [
   },
   {
     uuid: 'a6894d99-3369-4942-afe6-7b5d1712144f',
-    title: '36',
+    title: 'Amazon restaurant',
     description: '',
     src: assetGallery('exterior-interior/images/036.jpg'),
     thumbnailSrc: assetGallery('exterior-interior/thumbnails/036.jpg'),
@@ -291,7 +291,7 @@ export const ExteriorInteriors = [
   },
   {
     uuid: '53d9c025-c674-4167-929f-504cac2b4e73',
-    title: '37',
+    title: 'Amazon restaurant',
     description: '',
     src: assetGallery('exterior-interior/images/037.jpg'),
     thumbnailSrc: assetGallery('exterior-interior/thumbnails/037.jpg'),
@@ -299,7 +299,7 @@ export const ExteriorInteriors = [
   },
   {
     uuid: '98acbec3-23af-472b-8ff1-8358c64e50b4',
-    title: '38',
+    title: 'Amazon restaurant',
     description: '',
     src: assetGallery('exterior-interior/images/038.jpg'),
     thumbnailSrc: assetGallery('exterior-interior/thumbnails/038.jpg'),
@@ -307,7 +307,7 @@ export const ExteriorInteriors = [
   },
   {
     uuid: '1f86afde-b101-405b-832a-826c040a7848',
-    title: '39',
+    title: 'WaterWorld Complex\'s restaurant',
     description: '',
     src: assetGallery('exterior-interior/images/039.jpg'),
     thumbnailSrc: assetGallery('exterior-interior/thumbnails/039.jpg'),
