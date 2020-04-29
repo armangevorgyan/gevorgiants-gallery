b3e84719c9bc06c669c1a8cfbf2b0ddfb6b354e2
arman.gevorgyan <arman.gevorgyan@sflpro.com>
2020-04-30 01:04:56 +0400

    Change facebook meta description

--- app/src/components/shared/FacebookMetaTags/FacebookMetaTags.js
+++ app/src/components/shared/FacebookMetaTags/FacebookMetaTags.js
@@ -16,7 +16,7 @@ const FacebookMetaTags = ({
     <meta property="og:url" content={url || 'http://www.gevorgiants.com'} />
     <meta property="og:type" content={type || 'article'} />
     <meta property="og:title" content={title || translate('PAGE_MAIN_TITLE')} />
-    <meta property="og:description" content={description || 'Personal website of artist. Sculptures, Paintings and Interior Gallery'} />
+    <meta property="og:description" content={description || 'Personal website of Armen Gevorgiants. Sculptures, Paintings and Interior Gallery'} />
     <meta property="og:image" content={imgUrl || window.location.origin + asset('images/aniv.png')} />
   </Helmet>;
 
