0c4a33c6c632ada9b1b5bea0f140a2e6170c84de
arman.gevorgyan <arman.gevorgyan@sflpro.com>
2020-04-30 00:57:48 +0400

    Added default facebook met tags.

--- app/src/components/shared/FacebookMetaTags/FacebookMetaTags.js
+++ app/src/components/shared/FacebookMetaTags/FacebookMetaTags.js
@@ -16,8 +16,8 @@ const FacebookMetaTags = ({
     <meta property="og:url" content={url || 'http://www.gevorgiants.com'} />
     <meta property="og:type" content={type || 'article'} />
     <meta property="og:title" content={title || translate('PAGE_MAIN_TITLE')} />
-    <meta property="og:description" content={description || 'Armen Gevorinats\'s Gallery'} />
-    <meta property="og:image" content={imgUrl || asset('images/aniv.png')} />
+    <meta property="og:description" content={description || 'Personal website of artist. Sculptures, Paintings and Interior Gallery'} />
+    <meta property="og:image" content={imgUrl || window.location.origin + asset('images/aniv.png')} />
   </Helmet>;
 
 };
